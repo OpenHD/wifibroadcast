ec76cd81788142596205bef34d4f390cba32ee76
Stephen Oliver <steve@infincia.com>
2020-09-30 23:09:46 -0400

    Add OpenHD repository script

---
+++ package.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+PACKAGE_ARCH=$1
+OS=$2
+DISTRO=$3
+BUILD_TYPE=$4
+
+
+if [ "${BUILD_TYPE}" == "docker" ]; then
+    cat << EOF > /etc/resolv.conf
+options rotate
+options timeout:1
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+fi
+
+apt -y update || exit 1
+
+apt -y install libpcap-dev libsodium-dev || exit 1
+
+PACKAGE_NAME=wifibroadcast
+
+TMPDIR=/tmp/${PACKAGE_NAME}-installdir
+
+rm -rf ${TMPDIR}/*
+
+mkdir -p ${TMPDIR}/usr/local/bin/ || exit 1
+
+make clean || exit 1
+
+make all_bin || exit 1
+
+cp -a wfb_rx ${TMPDIR}/usr/local/bin/ || exit 1
+cp -a wfb_rx ${TMPDIR}/usr/local/bin/ || exit 1
+cp -a wfb_keygen ${TMPDIR}/usr/local/bin/ || exit 1
+
+VERSION=$(git describe)
+
+rm ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
+
+# dev packages are used as dependencies because some distros include version numbers in library
+# package names, but the -dev package is always the same and depends on the library package anyway
+fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TMPDIR} \
+  -p ${PACKAGE_NAME}_VERSION_ARCH.deb \
+  -d "libpcap-dev" \
+  -d "libsodium-dev" || exit 1
+
+#
+# Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
+# don't create a tag. You can build packages and test them locally without tagging.
+#
+git describe --exact-match HEAD > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    echo "Pushing package to OpenHD repository"
+    cloudsmith push deb openhd/openhd-2-1/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
+else
+    echo "Pushing package to OpenHD testing repository"
+    cloudsmith push deb openhd/openhd-2-1-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
+fi
