beb030bf82d55df29783619bb475a1a0cf74f886
consti10 <geierconstantinabc@gmail.com>
2023-08-09 11:38:45 +0200

    we need keypairs from pw at some point

--- executables/wfb_keygen.cpp
+++ executables/wfb_keygen.cpp
@@ -16,8 +16,6 @@
  *   with this program; if not, write to the Free Software Foundation, Inc.,
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-#include <stdio.h>
-#include <sodium.h>
 
 #include "../src/Encryption.hpp"
 
