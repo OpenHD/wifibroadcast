266a0b81eea0a4534d96646718792e09336158bc
consti10 <geierconstantinabc@gmail.com>
2023-08-10 20:03:39 +0200

    fix tests

--- executables/unit_test.cpp
+++ executables/unit_test.cpp
@@ -110,8 +110,10 @@ static void test_fec_stream_random_bs_fs_overhead_dropped(){
 // Test encryption+packet validation and packet validation only
 static void test_encrypt_decrypt_validate(const bool useGeneratedFiles,bool message_signing_only) {
   std::cout << "Using generated keypair (default seed otherwise):" << (useGeneratedFiles ? "y" : "n") << "\n";
-  std::optional<std::string> encKey = useGeneratedFiles ? std::optional<std::string>("../example_keys/gs.key") : std::nullopt;
-  std::optional<std::string> decKey = useGeneratedFiles ? std::optional<std::string>("../example_keys/drone.key") : std::nullopt;
+  const std::string filename_gs="gs.key"; //"../example_keys/gs.key"
+  const std::string filename_drone="drone.key" //"../example_keys/drone.key"
+  std::optional<std::string> encKey = useGeneratedFiles ? std::optional<std::string>(filename_gs) : std::nullopt;
+  std::optional<std::string> decKey = useGeneratedFiles ? std::optional<std::string>(filename_drone) : std::nullopt;
   if(message_signing_only){
     std::cout<<"Testing message signing\n";
   }else{
